83d3a262630f2fce54c9b4e301dd168e48eabc76
Felix Laniyan <flaniyanjr@gmail.com>
2023-10-23 16:49:11 -0400

    passed first test

---
+++ lib/deli_counter.py
@@ -0,0 +1,15 @@
+
+katz_deli = []
+
+def line(people_in_line):
+    if len(people_in_line) > 0:
+        print(f"The line is currently: {katz_deli}.") 
+    else:
+        print("The line is currently empty.") 
+
+def take_a_number():
+    pass
+
+def now_serving():
+    pass
+
