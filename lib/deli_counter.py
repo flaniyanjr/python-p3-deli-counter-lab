d8d4de358a884efc31952dc8bdb4435012d4727c
Emily Valdez <emily.milan.valdez@gmail.com>
2023-10-23 16:32:37 -0500

    in-class attempt

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -2,13 +2,15 @@
 katz_deli = []
 
 def line(people_in_line):
-    if len(people_in_line) > 0:
-        print(f"The line is currently: {katz_deli}.") 
-    else:
-        print("The line is currently empty.") 
+    for i in range(len(people_in_line)):
+        if len(people_in_line) > 0:
+            print(f"The line is currently: {i}.{people_in_line[i]}.") 
+        else:
+            print("The line is currently empty.") 
 
-def take_a_number():
-    pass
+def take_a_number(lst, str):
+    for person in lst:
+        print(f"Welcome, {str}. You are number {person}.{lst} in line.")
 
 def now_serving():
     pass
